811801a508b7cf56a222fc5aff124a54ea3cae3f
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-04-01 22:53:07 +0600

    added get api for fetching according to id, status, email

--- index.js
+++ index.js
@@ -46,12 +46,50 @@ async function run() {
             res.send(result)
         })
         
-        //User API
-        app.post('/user/:id', async (req, res) => {
-            const userId = req.params.id;
+        //Users API
+        app.post('/user', async (req, res) => {
             const data = req.body;
-            console.log(data)
+            // console.log(data)
             const result = await usersCollection.insertOne(data)
+            res.send(result)
+        })
+
+        app.get('/user', async (req, res) => {
+            const id = req.query.id;
+            const status = req.query.status
+            const email = req.query.email
+            let query={};
+            // console.log(email,id, status)
+            if(id){
+                 query._id =new ObjectId(id)
+            }
+            if(email){
+                query.userEmail= email
+            }
+            if(status){
+                query.AssignmentStatus = status
+            }
+            // console.log(query)
+            const result = await usersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.put('/user/:id',async(req,res)=>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = {upsert: true}
+            const updateData = req.body;
+            const updatedDoc = {
+                $set: updateData
+            }
+            const result = await usersCollection.updateOne(filter,updatedDoc,options)
+            res.send(result)
+        })
+
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
 
