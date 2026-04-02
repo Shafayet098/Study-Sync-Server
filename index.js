7941a3b5c472a43208d4bfbec5ef7a5eb53388c8
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-04-02 14:48:49 +0600

    added vercel.json for deployment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 //middleWare
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173',"https://study-sync-project-2d35a.web.app"],
     credentials: true
 }));
 app.use(express.json())
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const assignmentCollection = client.db('StudySync').collection('assignment')
         const usersCollection = client.db('StudySync').collection('users')
 
@@ -179,8 +179,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
