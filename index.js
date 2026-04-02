0d0592466f157075d1eb4b3406ee64b694b4f27e
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-04-02 12:47:25 +0600

    completed implementaion of jwt token varification process

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 3000;
 
 //middleWare
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
 
+//verify Token
+const verifyToken=(req,res,next)=>{
+    const token = req?.cookies?.token;
+    console.log('cookie in the middleware ',token)
+    if(!token){
+        return res.status(401).send({message:'unauthorized Access'})
+    }
+    //verify token
+    jwt.verify(token, process.env.JWT_Access_Secret,(err,decoded)=>{
+        if(err){
+            return res.status(401).send({message:'unauthorized Access'})
+        }
+        req.decoded = decoded
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4qasang.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -24,6 +46,30 @@ async function run() {
         await client.connect();
         const assignmentCollection = client.db('StudySync').collection('assignment')
         const usersCollection = client.db('StudySync').collection('users')
+
+        //jwt token related API
+        app.post('/jwt', (req, res) => {
+            const userData = req.body
+            const token = jwt.sign(userData, process.env.JWT_Access_Secret, { expiresIn: '1d' })
+
+            //set token in the cookies
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            })
+            res.send({ success: true })
+        })
+        //Clear token on logout
+        app.get('/logout', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+                maxAge: 0
+            })
+            res.send({ success: true })
+        })
 
         app.get('/assign', async (req, res) => {
             const cursor = assignmentCollection.find();
@@ -84,22 +130,29 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/userData/:email',verifyToken, async(req,res)=>{
+            const email = req.params.email;
+            if(email !== req.decoded.email){
+                 return res.status(403).send({message:'Forbidden Access'})
+            }
+            const query = {userEmail: email}
+            result = await usersCollection.find(query).toArray()
+            res.send(result)
+
+        })
         app.get('/user', async (req, res) => {
             const id = req.query.id;
             const status = req.query.status
-            const email = req.query.email
+            
             let query = {};
             // console.log(email,id, status)
             if (id) {
                 query._id = new ObjectId(id)
             }
-            if (email) {
-                query.userEmail = email
-            }
+            
             if (status) {
                 query.AssignmentStatus = status
             }
-            // console.log(query)
             const result = await usersCollection.find(query).toArray()
             res.send(result)
         })
@@ -123,56 +176,6 @@ async function run() {
             const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
-
-        // app.put('/coffees/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = {_id: new ObjectId(id)}
-        //     const options = { upsert: true };
-        //     const updatedCoffee = req.body;
-        //     const updatedDoc = {
-        //         $set: updatedCoffee
-        //     }
-        //     const result = await coffeesCollection.updateOne(filter, updatedDoc, options)
-        //     res.send(result)
-        // })
-
-        // app.delete('/coffees/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await coffeesCollection.deleteOne(query)
-        //     res.send(result)
-        // })
-        // //Users Related API
-        // //Sign Up Details
-        // app.get('/users', async (req, res) => {
-        //     const cursor = usersCollection.find();
-        //     const result = await cursor.toArray()
-        //     res.send(result);
-        // })
-        // app.post('/users', async (req, res) => {
-        //     const newUsers = req.body;
-        //     console.log(newUsers)
-        //     const result = await usersCollection.insertOne(newUsers)
-        //     res.send(result)
-        // })
-        // app.delete('/users/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await usersCollection.deleteOne(query)
-        //     res.send(result)
-        // })
-
-        // app.patch('/users',async(req, res)=>{
-        //     const {email, lastSignInTime} = req.body;
-        //     const filter = {email: email}
-        //     const updatedDoc={
-        //         $set: {
-        //             lastSignInTime: lastSignInTime
-        //         }
-        //     }
-        //     const result = await usersCollection.updateOne(filter, updatedDoc)
-        //     res.send(result)
-        // })
 
 
         // Send a ping to confirm a successful connection
