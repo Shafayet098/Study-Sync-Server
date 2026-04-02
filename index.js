201d45cc2bbd98f6e6eecd9ecc8cc22f2bcdacb4
Shafayet098 <shafayethossenchowdhury@gmail.com>
2026-04-02 10:33:15 +0600

    completed implementation of apis for filter, search, sort

--- index.js
+++ index.js
@@ -30,22 +30,52 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result);
         })
+        app.get('/cards', async (req, res) => {
+            const page = parseInt(req.query.page) - 1;
+            const size = parseInt(req.query.size);
+            const sort = req.query.sort;
+            const search = req.query.search
+            const filter = req.query.filter
+            let query = { title: { $regex: search, $options: 'i' } }
+            let options = {};
+            if (filter) {
+                query.category = { category: filter }
+            }
+            if (sort) {
+                options = { sort: { deadline: sort === 'asc' ? '1' : '-1' } }
+            }
+            const result = await assignmentCollection.find(query, options).skip(page * size).limit(size).toArray()
+            res.send(result)
+        })
+        app.get('/cards-count', async (req, res) => {
+            const search = req.query.search;
+            const filter = req.query.filter
+            let query = {}
+            if (search) {
+                query = { title: { $regex: search, $options: 'i' } }
+            }
+            if (filter) {
+                query.category = { category: filter }
+            }
+            const result = await assignmentCollection.countDocuments(query);
+            res.send(result)
+        })
 
         app.get('/assignment/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await assignmentCollection.findOne(query);
             console.log(result)
             res.send(result);
         })
-        
+
         app.post('/assign', async (req, res) => {
             const newAssign = req.body;
             console.log(newAssign)
             const result = await assignmentCollection.insertOne(newAssign)
             res.send(result)
         })
-        
+
         //Users API
         app.post('/user', async (req, res) => {
             const data = req.body;
@@ -58,15 +88,15 @@ async function run() {
             const id = req.query.id;
             const status = req.query.status
             const email = req.query.email
-            let query={};
+            let query = {};
             // console.log(email,id, status)
-            if(id){
-                 query._id =new ObjectId(id)
+            if (id) {
+                query._id = new ObjectId(id)
             }
-            if(email){
-                query.userEmail= email
+            if (email) {
+                query.userEmail = email
             }
-            if(status){
+            if (status) {
                 query.AssignmentStatus = status
             }
             // console.log(query)
@@ -74,21 +104,22 @@ async function run() {
             res.send(result)
         })
 
-        app.put('/user/:id',async(req,res)=>{
+
+        app.put('/user/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
-            const options = {upsert: true}
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
             const updateData = req.body;
             const updatedDoc = {
                 $set: updateData
             }
-            const result = await usersCollection.updateOne(filter,updatedDoc,options)
+            const result = await usersCollection.updateOne(filter, updatedDoc, options)
             res.send(result)
         })
 
         app.delete('/user/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await usersCollection.deleteOne(query)
             res.send(result)
         })
@@ -130,7 +161,7 @@ async function run() {
         //     const result = await usersCollection.deleteOne(query)
         //     res.send(result)
         // })
-        
+
         // app.patch('/users',async(req, res)=>{
         //     const {email, lastSignInTime} = req.body;
         //     const filter = {email: email}
@@ -154,9 +185,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('User server is running')
 })
-app.listen(port, ()=>{
-    console.log('Server is running on port ',port)
+app.listen(port, () => {
+    console.log('Server is running on port ', port)
 })
